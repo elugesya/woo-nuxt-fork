0ff99ed31b8bde0008736cce58e9ca0367d8db32
Emrah <eryilmaz.e@gmail.com>
2025-10-23 14:12:44 +0300

    categories populating issues solved.

--- woonuxt_base/app/pages/urun-kategorisi/[slug].vue
+++ woonuxt_base/app/pages/urun-kategorisi/[slug].vue
@@ -3,9 +3,11 @@ const { setProducts, updateProductList } = useProducts();
 const { isQueryEmpty } = useHelpers();
 const { storeSettings } = useAppConfig();
 const route = useRoute();
-const slug = route.params.slug;
 
-const { data } = await useAsyncGql('getProducts', { slug });
+const categorySlug = (route.params.categorySlug || route.params.slug) as string;
+
+const { data } = await useAsyncGql('getProductsByCategory', { categorySlug });
+
 const productsInCategory = (data.value?.products?.nodes || []) as Product[];
 setProducts(productsInCategory);
 
@@ -16,7 +18,7 @@ onMounted(() => {
 watch(
   () => route.query,
   () => {
-    if (route.name !== 'product-category-slug') return;
+    if (route.name !== 'product-category-page' && route.name !== 'product-category-page-pager') return;
     updateProductList();
   },
 );
@@ -28,16 +30,21 @@ useHead({
 </script>
 
 <template>
-  <div class="container flex items-start gap-16" v-if="productsInCategory.length">
-    <Filters v-if="storeSettings.showFilters" :hide-categories="true" />
+  <main>
+    <div class="container flex items-start gap-16" v-if="productsInCategory.length">
+      <Filters v-if="storeSettings.showFilters" :hide-categories="true" />
 
-    <div class="w-full">
-      <div class="flex items-center justify-between w-full gap-4 mt-8 md:gap-8">
-        <ProductResultCount />
-        <OrderByDropdown class="hidden md:inline-flex" v-if="storeSettings.showOrderByDropdown" />
-        <ShowFilterTrigger v-if="storeSettings.showFilters" class="md:hidden" />
+      <div class="w-full">
+        <div class="flex items-center justify-between w-full gap-4 mt-8 md:gap-8">
+          <ProductResultCount />
+          <OrderByDropdown class="hidden md:inline-flex" v-if="storeSettings.showOrderByDropdown" />
+          <ShowFilterTrigger v-if="storeSettings.showFilters" class="md:hidden" />
+        </div>
+        <ProductGrid />
       </div>
-      <ProductGrid />
     </div>
-  </div>
+    <div v-else class="container my-8 text-center">
+      <p class="text-gray-500">Bu kategoride ürün bulunamadı.</p>
+    </div>
+  </main>
 </template>
