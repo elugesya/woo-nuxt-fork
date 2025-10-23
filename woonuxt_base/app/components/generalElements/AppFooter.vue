f44b6b16f70843edb1fa780ac98d3a5230f13622
Emrah <eryilmaz.e@gmail.com>
2025-10-23 15:46:37 +0300

    monolang

--- woonuxt_base/app/components/generalElements/AppFooter.vue
+++ woonuxt_base/app/components/generalElements/AppFooter.vue
@@ -9,7 +9,6 @@ const { wishlistLink } = useAuth();
       <div class="mr-auto">
         <Logo />
         <WebsiteShortDescription />
-        <LangSwitcher class="mt-8" />
       </div>
       <div class="w-3/7 lg:w-auto">
         <div class="mb-1 font-semibold">{{ $t('general.information') }}</div>
