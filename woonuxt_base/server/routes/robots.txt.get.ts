4d34fe11c11b5f2fa2be7fe4f96a6200ecc062e9
Emrah <eryilmaz.e@gmail.com>
2025-10-26 16:57:32 +0300

    ss

--- woonuxt_base/server/routes/robots.txt.get.ts
+++ woonuxt_base/server/routes/robots.txt.get.ts
@@ -9,36 +9,18 @@ export default defineEventHandler((event) => {
     const host = (headers['x-forwarded-host'] as string) || (headers['host'] as string) || 'localhost:3000'
     SITE_URL = `${proto}://${host}`
   }
-  // Consider it production if NODE_ENV is production OR FRONT_END_URL points to a non-localhost domain
-  const feUrl = ((config.public as any).FRONT_END_URL as string | undefined) || ''
-  const looksLikeProd = /^https?:\/\/(?!localhost|127\.0\.0\.1)([^\s]+)$/i.test(feUrl)
-  const isProd = process.env.NODE_ENV === 'production' || looksLikeProd
 
-  const common = [
+  // Always allow indexing - no conditions
+  const rules = [
     'User-agent: *',
+    'Allow: /',
+    'Disallow: /odeme/',
+    'Disallow: /siparis-ozeti/',
+    'Disallow: /hesabim/',
+    'Disallow: /oauth/',
+    '',
+    `Sitemap: ${SITE_URL}/sitemap.xml`,
   ]
-
-  let rules: string[] = []
-  if (isProd) {
-    rules = [
-      ...common,
-      'Allow: /',
-      'Disallow: /odeme/',
-      'Disallow: /siparis-ozeti/',
-      'Disallow: /hesabim/',
-      'Disallow: /oauth/',
-      '',
-      `Sitemap: ${SITE_URL}/sitemap.xml`,
-    ]
-  } else {
-    // Prevent indexing in development
-    rules = [
-      ...common,
-      'Disallow: /',
-      '',
-      `Sitemap: ${SITE_URL}/sitemap.xml`,
-    ]
-  }
 
   const body = rules.join('\n')
   setHeader(event, 'Content-Type', 'text/plain; charset=UTF-8')
